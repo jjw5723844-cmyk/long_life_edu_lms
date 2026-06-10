44e9d3b2112927e33f9e2b57f5f81be056ebcbf8
jjw5723844-cmyk <jjw5723844@gamil.com>
2026-06-10 14:05:08 +0900

    코드 수정

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,16 @@
 class CoursesController < ApplicationController
+  # 액션 실행전 @course 자동 조회
   before_action :set_course, only: %i[ show edit update destroy ]
 
-  # GET /courses or /courses.json
+  # 1. 개설된 모든 강좌들을 가져오는 액션
   def index
     @courses = Course.all
   end
 
+  # 2. 특정 혹은 키워드별 강좌 하나를 가져오는 세부 액션
   # GET /courses/1 or /courses/1.json
   def show
+    @course = Course.find(params[:id])
   end
 
   # GET /courses/new
@@ -15,46 +18,33 @@ class CoursesController < ApplicationController
     @course = Course.new
   end
 
-  # GET /courses/1/edit
+  # 수정 폼 화면을 보여주는 액션
   def edit
   end
 
-  # POST /courses or /courses.json
+  # 강좌 생성 액션
   def create
     @course = Course.new(course_params)
-
-    respond_to do |format|
-      if @course.save
-        format.html { redirect_to @course, notice: "Course was successfully created." }
-        format.json { render :show, status: :created, location: @course }
-      else
-        format.html { render :new, status: :unprocessable_content }
-        format.json { render json: @course.errors, status: :unprocessable_content }
-      end
+    if @course.save
+      redirect_to @course, notice: "강좌가 성공적으로 개설되었습니다."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
+  # 강의 데이터 수정(DB 연동)
   def update
-    respond_to do |format|
-      if @course.update(course_params)
-        format.html { redirect_to @course, notice: "Course was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @course }
-      else
-        format.html { render :edit, status: :unprocessable_content }
-        format.json { render json: @course.errors, status: :unprocessable_content }
-      end
+    if @course.update(course_params)
+      redirect_to @course, notice: "강좌 정보가 성공적으로 수정되었습니다."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
+  # 강좌 삭제
   def destroy
-    @course.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to courses_path, notice: "Course was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    @course.destroy
+    redirect_to course_path, notice: "강좌가 성공적으로 폐강(삭제)되었습니다."
   end
 
   private
@@ -63,7 +53,7 @@ class CoursesController < ApplicationController
       @course = Course.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
+    # 스트롱 파라미터를 통해 허용할 컬럼들을 DB 구조에 맞게 확인
     def course_params
       params.expect(course: [ :title, :description, :instructor_name, :max_students, :category_id ])
     end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, optional: true
 
   has_many :registrations
   has_many :users, through: :registrations
