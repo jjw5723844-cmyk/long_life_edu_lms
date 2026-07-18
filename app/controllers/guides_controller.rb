729b98d07bcc5965c2c3945c4a61cd590fa27dd2
jjw <jjw5723844@gmail.com>
2026-07-18 17:18:16 +0900

    이용안내 도메인 페이지 완성 및 비회원 예외처리 설정

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,8 @@
 class GuidesController < ApplicationController
+  # 이용안내 비회원 등 불특정 다수의 인원도 자유롭게 볼 수 있도록 설정
+  # 상위 보안 인증 필터 해제(raise: false로 오류 방지)
+  allow_unauthenticated_access only: [ :index, :registration, :facility ], raise: false
+
   # 이용안내 메인 종합 허브(자주 묻는 질문, FAQ 등을 포함)
   def index
   end
